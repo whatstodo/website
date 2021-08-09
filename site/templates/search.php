e4ae6e82a3cd98906ba4b15ebe101cd0ad99082e
belameiers <post@belameiers.de>
2021-08-09 12:07:17 +0200

    Search Improvement + Search Preview

--- site/templates/search.php
+++ site/templates/search.php
@@ -1,62 +1,69 @@
 <?php snippet('header'); ?>
 
 
+<!-- Only display a result for search terms more than 2 characters -->
 <?php if (strlen($query) <= 2): ?> 
 
-  <div class="caption">Mindestens 2 Buchstaben</div>
+    <div class="caption">Mindestens 2 Buchstaben</div>
 
   <?php else: ?>
 
-  <?php if ($results->isEmpty() && $resultsPos->isEmpty() && $resultsPart->isEmpty()): ?> 
+    <!-- Display "no result" for unseccesfull searches -->
+    <?php if ($results->isEmpty() && $resultsPos->isEmpty() && $resultsPart->isEmpty()): ?> 
 
-    <div class="caption">Es konnten leider keine Ergebnisse für "<span><?= html($query) ?> </span>" gefunden werden!</div>
+      <div class="caption">Es konnten leider keine Ergebnisse für "<span><?= html($query) ?> </span>" gefunden werden!</div>
 
-    <?php else: ?>
+      <?php else: ?>
 
-      <div class="caption"><h2><?= $page->title() ?></h2> für "<span><?= html($query) ?> </span>"</div>
+        <!-- Display results for searches categorised in 3 Parts-->
+        <div class="caption"><h2><?= $page->title() ?></h2> für "<span><?= html($query) ?> </span>"</div>
 
-      <?php if ($results->isNotEmpty()): ?>
-        <h3>Allgemein</h3>
-        <ul>
-          <?php foreach ($results as $result): ?>
-            <li>
-              <a href="<?= $result->url() ?>">
-                <?= $result->title() ?>
-              </a>
-            </li>
-          <?php endforeach ?>
-        </ul>
-      <?php endif ?>
+        <!-- Searchresults in general pages  -->
+        <?php if ($results->isNotEmpty()): ?>
+          <h3>Allgemein</h3>
+          <ul>
+            <?php foreach ($results as $result): ?>
+              <li>
+                <a href="<?= $result->url() ?>">
+                  <?= $result->title() ?>
+                </a>
+                <!-- Display the textarea where the searchterm was found -->
+                <?= getResultText($result->text(), $query) ?>
+              </li>
+            <?php endforeach ?>
+          </ul>
+        <?php endif ?>
 
-      <?php if ($resultsPos->isNotEmpty()): ?>
-        <h3>Positionen</h3>
-        <ul>
-          <?php foreach ($resultsPos as $result): ?>
-            <li>
-              <a href="<?= $result->url() ?>">
-                <?= $result->title() ?>
-              </a>
-            </li>
-          <?php endforeach ?>
-        </ul>
-      <?php endif ?>
+        <!-- Searchresults in positions  -->
+        <?php if ($resultsPos->isNotEmpty()): ?>
+          <h3>Positionen</h3>
+          <ul>
+            <?php foreach ($resultsPos as $result): ?>
+              <li>
+                <a href="<?= $result->url() ?>">
+                  <?= $result->title() ?>
+                </a><br>
+              </li>
+            <?php endforeach ?>
+          </ul>
+        <?php endif ?>
 
-      <?php if ($resultsPart->isNotEmpty()): ?>
-        <h3>Teilnehmende</h3>
-        <ul>
-          <?php foreach ($resultsPart as $result): ?>
-            <li>
-              <!-- Link needs to be adjusted! -->
-              <a href="<?= $result->url() ?>">
-                <?= $result->name() ?>
-              </a>
-            </li>
-          <?php endforeach ?>
-        </ul>
-      <?php endif ?>
+        <!-- Searchresults in Participants  -->
+        <?php if ($resultsPart->isNotEmpty()): ?>
+          <h3>Teilnehmende</h3>
+          <ul>
+            <?php foreach ($resultsPart as $result): ?>
+              <li>
+                <a href="<?= $result->url() ?>">
+                  <?= $result->name() ?>
+                </a>
+              </li>
+            <?php endforeach ?>
+          </ul>
+        <?php endif ?>
 
+    <?php endif ?>
   <?php endif ?>
-<?php endif ?>
 
 
   
