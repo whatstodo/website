0fa69ec00dd89582c90890adbf6b3a15c79ee5df
arnoson <hallo@arno-schlipf.info>
2021-08-17 12:33:34 +0200

    Don't use cache
    There are so many dynamic pages that it will be difficult the esclude
    them all from caching.

--- site/config/config.php
+++ site/config/config.php
@@ -4,13 +4,4 @@ return [
   'debug' => true,
   'languages' => true,
   'languages.detect' => true,
-  // Deactivate Kirbys Cache because of "Kirby Uniform"
-  'cache' => [
-    'pages' => [
-      'active' => false,
-      'ignore' => function ($page) {
-        return $page->title()->value() === 'Do not cache me';
-      },
-    ],
-  ],
 ];
