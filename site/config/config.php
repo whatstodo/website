4f4f160c615e6b2617f731974e4941ab4c8bacae
arnoson <hallo@arno-schlipf.info>
2021-08-17 13:47:18 +0200

    Add logout

--- site/config/config.php
+++ site/config/config.php
@@ -4,4 +4,20 @@ return [
   'debug' => true,
   'languages' => true,
   'languages.detect' => true,
+  'routes' => [
+    [
+      'pattern' => 'logout',
+      'language' => '*',
+      'action' => function () {
+        // For the logout we don’t need a real page. A simple URL to send
+        // logged-in users to is enough.
+
+        if ($user = kirby()->user()) {
+          $user->logout();
+        }
+
+        go('login');
+      },
+    ],
+  ],
 ];
